18c665f4d444d8ad3f870109affdab50f221efaa
右田和也”git config --global user.email kzy.migita@gmail.com” <migitakazuya@udakazuyanoMacBook-Air.local>
2025-12-23 12:12:27 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-red-400 to-yellow-200">
+      <h1 className="text-4xl font-semibold text-gray-800">
+        評価の部屋
+      </h1>
+    </main>
+  );
+}
